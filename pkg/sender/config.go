80b3333b51af265a39ddfbc3ba94fb3c0299e1cb
Aya (Igarashi) Ozawa <Ladiclexxx@gmail.com>
2024-09-27 08:37:51 +0900

    Add usage sender (#5)

---
+++ pkg/sender/config.go
@@ -0,0 +1,62 @@
+package sender
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	defaultInitialDelay = 5 * time.Second
+	defaultInterval     = 10 * time.Second
+
+	defaultUsageChannelSize = 300
+
+	// defaultMaxMessageSize is the default maximum size of a message.
+	// This value considers the default Kafka message size limit.
+	defaultMaxMessageSize = 768 * 1024
+)
+
+// Config is the configuration for the sender.
+type Config struct {
+	// ServerAddr is the address of the server to send usage data to.
+	ServerAddr string `yaml:"serverAddr"`
+	// InitialDelay is the time to wait before starting the sender.
+	InitialDelay time.Duration `yaml:"initialDelay"`
+	// Interval is the interval at which the sender sends usage data to the server.
+	Interval time.Duration `yaml:"interval"`
+	// UsageChannelSize is the size of the channel that stores usage data.
+	UsageChannelSize int `yaml:"usageChannelSize"`
+	// MaxMessageSize is the maximum size of a message that the sender can send one gRPC call.
+	MaxMessageSize int `yaml:"maxMessageSize"`
+}
+
+// Validate validates the configuration.
+func (c *Config) Validate() error {
+	if c.ServerAddr == "" {
+		return fmt.Errorf("server address is required")
+	}
+
+	if c.InitialDelay == 0 {
+		c.InitialDelay = defaultInitialDelay
+	} else if c.InitialDelay < 0 {
+		return fmt.Errorf("initial delay must be greater than 0")
+	}
+	if c.Interval == 0 {
+		c.Interval = defaultInterval
+	} else if c.Interval < 0 {
+		return fmt.Errorf("interval must be greater than 0")
+	}
+
+	if c.UsageChannelSize == 0 {
+		c.UsageChannelSize = defaultUsageChannelSize
+	} else if c.UsageChannelSize < 0 {
+		return fmt.Errorf("usage channel size must be greater than 0")
+	}
+
+	if c.MaxMessageSize == 0 {
+		c.MaxMessageSize = defaultMaxMessageSize
+	} else if c.MaxMessageSize < 0 {
+		return fmt.Errorf("max message size must be greater than 0")
+	}
+	return nil
+}
