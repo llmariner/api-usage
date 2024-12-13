f83e43d9783676fae6650a02e1907d3a08ca7e12
Guangrui Fu <guangrui@cloudnatix.com>
2024-12-13 20:44:58 +0000

    feat(store): get usages grouped by api key, user and model

--- server/internal/store/usage.go
+++ server/internal/store/usage.go
@@ -45,3 +45,37 @@ func (s *Store) FindUsages() ([]*Usage, error) {
 	}
 	return usages, nil
 }
+
+// UsageByGroup represents the aggregated usage data grouped by API key, user, and model.
+type UsageByGroup struct {
+	APIKeyID string
+	UserID   string
+	ModelID  string
+
+	TotalRequests           int64
+	TotalPromptTokens       int64
+	TotalCompletionTokens   int64
+	AverageLatency          float64
+	AverageTimeToFirstToken float64
+}
+
+// GetUsagesByGroups returns the aggregated usage data by groups for the given tenant and time range.
+func (s *Store) GetUsagesByGroups(tenantID string, start, end int64) ([]*UsageByGroup, error) {
+	var us []*UsageByGroup
+	if err := s.db.Model(&Usage{}).
+		Select(
+			`api_key_id`, `user_id`, `model_id`,
+			`COUNT(*) AS total_requests`,
+			`AVG(time_to_first_token_ms) AS average_time_to_first_token`,
+			`SUM(prompt_tokens) AS total_prompt_tokens`,
+			`SUM(completion_tokens) AS total_completion_tokens`,
+			`AVG(latency_ms) AS average_latency`).
+		Where("tenant = ?", tenantID).
+		Where("timestamp >= ? AND timestamp < ?", start, end).
+		Group("api_key_id,user_id,model_id").
+		Order("api_key_id,user_id,model_id").
+		Scan(&us).Error; err != nil {
+		return nil, err
+	}
+	return us, nil
+}
---
+++ server/internal/store/usage_test.go
@@ -0,0 +1,181 @@
+package store
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUsagesByGroups(t *testing.T) {
+	st, tearDown := NewTest(t)
+	defer tearDown()
+
+	start := int64(1610000000)
+	end := int64(1610000200)
+	usages := []*Usage{
+		{
+			Tenant:     "t0",
+			APIMethod:  "GetFoo",
+			StatusCode: 200,
+			Timestamp:  start,
+			LatencyMS:  100,
+
+			UserID:   "u0",
+			APIKeyID: "api_key0",
+			ModelID:  "model0",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       100,
+			CompletionTokens:   100,
+		},
+		{
+			Tenant:     "t0",
+			APIMethod:  "GetFoo",
+			StatusCode: 200,
+			Timestamp:  start + 50,
+			LatencyMS:  150,
+
+			UserID:   "u0",
+			APIKeyID: "api_key0",
+			ModelID:  "model1",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       100,
+			CompletionTokens:   100,
+		},
+		{
+			Tenant:     "t0",
+			APIMethod:  "GetFoo",
+			StatusCode: 404,
+			Timestamp:  start + 100,
+			LatencyMS:  200,
+
+			UserID:   "u1",
+			APIKeyID: "api_key0",
+			ModelID:  "model0",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       100,
+			CompletionTokens:   100,
+		},
+		{
+			Tenant:     "t0",
+			APIMethod:  "UpdateFoo",
+			StatusCode: 200,
+			Timestamp:  start + 150,
+			LatencyMS:  300,
+
+			UserID:   "u1",
+			APIKeyID: "api_key1",
+			ModelID:  "model0",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       100,
+			CompletionTokens:   100,
+		},
+		{
+			Tenant:     "t0",
+			APIMethod:  "UpdateFoo",
+			StatusCode: 200,
+			Timestamp:  start + 190,
+			LatencyMS:  100,
+
+			UserID:   "u1",
+			APIKeyID: "api_key1",
+			ModelID:  "model0",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       100,
+			CompletionTokens:   100,
+		},
+		// out of range
+		{
+			Tenant:     "t0",
+			APIMethod:  "UpdateFoo",
+			StatusCode: 200,
+			Timestamp:  start + 200,
+			LatencyMS:  100,
+
+			UserID:   "u0",
+			APIKeyID: "api_key0",
+			ModelID:  "model0",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       1000,
+			CompletionTokens:   1000,
+		},
+		// different tenant
+		{
+			Tenant:     "t1",
+			APIMethod:  "GetFoo",
+			StatusCode: 404,
+			Timestamp:  start + 130,
+			LatencyMS:  200,
+
+			UserID:   "u10",
+			APIKeyID: "api_key10",
+			ModelID:  "model0",
+
+			TimeToFirstTokenMS: 10,
+			PromptTokens:       100,
+			CompletionTokens:   100,
+		},
+	}
+	err := st.CreateUsage(usages...)
+	assert.NoError(t, err)
+
+	result, err := st.GetUsagesByGroups("t0", start, end)
+	assert.NoError(t, err)
+	assert.Len(t, result, 4)
+
+	us := []*UsageByGroup{
+		{
+			APIKeyID: "api_key0",
+			UserID:   "u0",
+			ModelID:  "model0",
+
+			TotalRequests:           1,
+			TotalPromptTokens:       100,
+			TotalCompletionTokens:   100,
+			AverageLatency:          100,
+			AverageTimeToFirstToken: 10,
+		},
+		{
+			APIKeyID: "api_key0",
+			UserID:   "u0",
+			ModelID:  "model1",
+
+			TotalRequests:           1,
+			TotalPromptTokens:       100,
+			TotalCompletionTokens:   100,
+			AverageLatency:          150,
+			AverageTimeToFirstToken: 10,
+		},
+		{
+			APIKeyID: "api_key0",
+			UserID:   "u1",
+			ModelID:  "model0",
+
+			TotalRequests:           1,
+			TotalPromptTokens:       100,
+			TotalCompletionTokens:   100,
+			AverageLatency:          200,
+			AverageTimeToFirstToken: 10,
+		},
+		{
+			APIKeyID: "api_key1",
+			UserID:   "u1",
+			ModelID:  "model0",
+
+			TotalRequests:           2,
+			TotalPromptTokens:       200,
+			TotalCompletionTokens:   200,
+			AverageLatency:          200,
+			AverageTimeToFirstToken: 10,
+		},
+	}
+
+	for i, res := range result {
+		assert.Equal(t, us[i], res)
+	}
+}
