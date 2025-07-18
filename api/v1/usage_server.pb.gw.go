c73186180c14bcda42a57a292eaa2003c1a37923
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-07-17 18:28:14 -0700

    feat(api): add a new API for listing model usages

--- api/v1/usage_server.pb.gw.go
+++ api/v1/usage_server.pb.gw.go
@@ -67,6 +67,42 @@ func local_request_APIUsageService_ListUsageData_0(ctx context.Context, marshale
 
 }
 
+var (
+	filter_APIUsageService_ListModelUsageSummaries_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_APIUsageService_ListModelUsageSummaries_0(ctx context.Context, marshaler runtime.Marshaler, client APIUsageServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListModelUsageSummariesRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_APIUsageService_ListModelUsageSummaries_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.ListModelUsageSummaries(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_APIUsageService_ListModelUsageSummaries_0(ctx context.Context, marshaler runtime.Marshaler, server APIUsageServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListModelUsageSummariesRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_APIUsageService_ListModelUsageSummaries_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.ListModelUsageSummaries(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterAPIUsageServiceHandlerServer registers the http handlers for service APIUsageService to "mux".
 // UnaryRPC     :call APIUsageServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -93,6 +129,29 @@ func RegisterAPIUsageServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		}
 
 		forward_APIUsageService_ListUsageData_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_APIUsageService_ListModelUsageSummaries_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/llmariner.apiusage.server.v1.APIUsageService/ListModelUsageSummaries", runtime.WithHTTPPathPattern("/v1/api-usage/model-usage-summaries"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_APIUsageService_ListModelUsageSummaries_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_APIUsageService_ListModelUsageSummaries_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -157,13 +216,37 @@ func RegisterAPIUsageServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("GET", pattern_APIUsageService_ListModelUsageSummaries_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/llmariner.apiusage.server.v1.APIUsageService/ListModelUsageSummaries", runtime.WithHTTPPathPattern("/v1/api-usage/model-usage-summaries"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_APIUsageService_ListModelUsageSummaries_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_APIUsageService_ListModelUsageSummaries_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_APIUsageService_ListUsageData_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "api_usages"}, ""))
+
+	pattern_APIUsageService_ListModelUsageSummaries_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "api-usage", "model-usage-summaries"}, ""))
 )
 
 var (
 	forward_APIUsageService_ListUsageData_0 = runtime.ForwardResponseMessage
+
+	forward_APIUsageService_ListModelUsageSummaries_0 = runtime.ForwardResponseMessage
 )
